cc65f67953009f33cf55d1e9b2b028e57d4a7d6b
Alymbekov <maxim.makarov.1997@mail.ru>
2020-03-30 20:46:09 +0600

    create index view for load indexpage with CBV

--- board_posts/views.py
+++ board_posts/views.py
@@ -4,5 +4,15 @@ from .models import Post
 class IndexPageView(ListView):
     model = Post
     template_name = 'index.html'
+    context_object_name = "posts"
 
-    
+    #this is example, how to work ListView
+    """
+    def get_queryset(self):
+        Post.objects.all()
+        return super().get_queryset()
+    """
+    # example sql query
+    #SELECT text FROM Post;
+    # context_object_name = "object_list"
+    # {"object_list": Post.objects.all()}
