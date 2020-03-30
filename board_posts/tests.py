2e14113bb9eba0482cf29f42fdde9a1d73eff1f5
Alymbekov <maxim.makarov.1997@mail.ru>
2020-03-30 20:45:23 +0600

    add 4 test functionality

--- board_posts/tests.py
+++ board_posts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class PostTableTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='This is only for test')
+
+    
+    def test_post_content(self):
+        post = Post.objects.get(pk=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'This is only for test')
+
+class IndexPageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="second test")
+    
+    def test_view_url_exists_at_proper_location(self):
+        response = self.client.get('/') #http://localhost:8000/
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_url_by_name(self):
+        # build path 
+        #http://localhost:8000/
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('index')) #http://localhost:8000/
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')  
